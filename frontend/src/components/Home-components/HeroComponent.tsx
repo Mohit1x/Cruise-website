581431af84e4ef79c274bc085022169059238ebf
Mohit1x <mohitvyas746@gmail.com>
2025-08-04 15:35:17 +0530

    added new hero image for desktop

--- frontend/src/components/Home-components/HeroComponent.tsx
+++ frontend/src/components/Home-components/HeroComponent.tsx
@@ -10,23 +10,25 @@ const heroImages = ["/hero-banner-1.png", "/hero-banner-2.png"];
 
 export default function HeroComponent() {
   return (
-    <div className="relative w-full md:aspect-[3/2] overflow-hidden rounded">
+    <div className="relative w-full h-64 md:h-auto overflow-hidden rounded-lg">
+      {/* Blurred background for desktop */}
       <img
         src="/hero-banner-1.png"
         alt="Blurred Background"
         className="absolute top-0 left-0 w-full h-full object-cover filter blur-lg scale-110 z-0 hidden md:block"
       />
 
-      <div className="relative w-full h-full z-10 px-2 rounded overflow-hidden">
-        <Carousel>
-          <CarouselContent>
+      {/* Carousel container */}
+      <div className="relative w-full h-full md:h-auto z-10 rounded-lg overflow-hidden">
+        <Carousel className="w-full h-full md:h-auto">
+          <CarouselContent className="h-full md:h-auto">
             {heroImages.map((src, index) => (
-              <CarouselItem key={index}>
-                <div className="w-full h-full flex items-center justify-center">
+              <CarouselItem key={index} className="h-full md:h-auto">
+                <div className="w-full h-full md:h-auto flex items-center justify-center p-2 md:p-0">
                   <img
                     src={src}
                     alt={`Hero Banner ${index + 1}`}
-                    className="w-full h-full object-contain object-center rounded-inherit"
+                    className="max-w-full max-h-full md:w-full md:h-auto object-contain md:object-cover rounded-md md:rounded-lg shadow-lg md:shadow-none"
                   />
                 </div>
               </CarouselItem>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,11 +2,16 @@ import { otherPortsData, propularDestination } from "@/constansts/utils";
 import HeroComponent from "../components/Home-components/HeroComponent";
 import CarouselComponent from "@/components/Home-components/CarouselComponent";
 import { OtherPorts } from "@/components/Home-components/OtherPorts";
+import { NewHeroComponent } from "@/components/Home-components/NewHeroComponent";
 
 const HomePage = () => {
   return (
     <div className="">
-      <HeroComponent />
+      <div className="block md:hidden"><HeroComponent /></div>
+      <div className="hidden md:block">
+      <NewHeroComponent/>
+
+      </div>
       <div className="px-5 md:max-w-6xl mx-auto flex flex-col gap-5 py-10">
         <CarouselComponent
           tag="Popular Destinations"
