83097f79f9f15ec5808f6db41819097292cec083
Mohit1x <mohitvyas746@gmail.com>
2025-08-06 18:43:23 +0530

    added mobile footer navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import { CruisePage } from "./pages/CruisePage";
 import { AuthPage } from "./pages/AuthPages";
 import AccountPage from "./pages/AccountPage";
+import GoOnTrip from "./pages/GoOnTrip";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/find-a-cruise/:id" element={<CruisePage />} />
+            <Route path="/go-to-a-trip" element={<GoOnTrip />} />
           </Route>
             <Route path="/account" element={<AccountPage />} />
         </Routes>
---
+++ frontend/src/components/MobileFooter.tsx
@@ -0,0 +1,35 @@
+import { AiFillHome } from "react-icons/ai";
+import { GiSailboat } from "react-icons/gi";
+import { Link, useLocation } from "react-router";
+
+export const MobileFooter = () => {
+  const location = useLocation();
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-white border-t-2 flex items-center md:hidden z-50">
+      <div className="w-1/2 text-center py-3">
+       <Link to={"/"}>
+        <div
+          className={`flex flex-col items-center justify-center ${
+            location.pathname === "/" ? "text-black" : "text-gray-400"
+          }`}
+        >
+          <AiFillHome size={24} />
+          <span>Home</span>
+        </div>
+       </Link>
+      </div>
+      <div className="w-1/2 text-center py-3">
+        <Link to={"/go-to-a-trip"}>
+        <div
+          className={`flex flex-col items-center justify-center ${
+            location.pathname === "/go-to-a-trip" ? "text-black" : "text-gray-400"
+          }`}
+        >
+          <GiSailboat size={24} />
+          <span>Go on a trip</span>
+        </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
