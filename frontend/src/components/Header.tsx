f6a11145f610235624f2d603946d85af1dc2ab9e
Mohit1x <mohitvyas746@gmail.com>
2025-08-12 19:21:03 +0530

    added link for admin panel

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,6 +18,7 @@ const Header = () => {
               CRUISE
             </div>
             <div className="flex items-center gap-2 md:gap-5">
+            <Link to={"/admin/dashboard"}><h1>Admin</h1></Link>
             <img src="/spin-icon.png" className="h-8 w-8 md:h-12 md:w-12" onClick={()=>setIsModalOpen(true)}/>
             <img src="/shield-icon.png" className="h-8 w-8 md:h-12 md:w-12"/>
             <img src="/message-icon.png" className="h-8 w-8 md:h-12 md:w-12"/>
